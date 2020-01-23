709bd57c92b6a40a49b1ba2482fa3e064f5227fc
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-01-23 11:41:05 +0100

    Add project files.

---
+++ Exercise1/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public string Fullname => $"{Firstname} {Lastname}";
+    }
+}
